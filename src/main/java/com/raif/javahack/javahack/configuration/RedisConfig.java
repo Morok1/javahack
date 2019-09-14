24da6c14791ad0e1bbedd2635fadb88d66d00c2e
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-09-14 21:56:09 +0300

    uncomment dependencies for redis

--- src/main/java/com/raif/javahack/javahack/configuration/RedisConfig.java
+++ src/main/java/com/raif/javahack/javahack/configuration/RedisConfig.java
@@ -1,18 +1,21 @@
 package com.raif.javahack.javahack.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
-//@Configuration
+@Configuration
 public class RedisConfig {
-//    @Bean
-//    JedisConnectionFactory jedisConnectionFactory() {
-//        return new JedisConnectionFactory();
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(jedisConnectionFactory());
-//        return template;
-//    }
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
 }
