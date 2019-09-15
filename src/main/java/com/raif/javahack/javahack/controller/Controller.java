a7a61277bab59ca585283bd448105d8023086347
Якимчиков Максим Викторович <Yakimchikov.M.V@sberbank.ru>
2019-09-15 08:42:12 +0300

    simple integration with redis

--- src/main/java/com/raif/javahack/javahack/controller/Controller.java
+++ src/main/java/com/raif/javahack/javahack/controller/Controller.java
@@ -1,4 +1,21 @@
 package com.raif.javahack.javahack.controller;
 
+import com.raif.javahack.javahack.Example;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
 public class Controller {
+    @Autowired
+    Example example;
+
+    @RequestMapping("/")
+    public Set<String> greeting(@RequestParam(value="okved", required=false) String okved) {
+        example.setProducerList(okved,new String[] {"Vera","Nadezda", "Lubov"});
+        return example.getProducerList(okved);
+    }
 }
