c30bf734a6e1c0fb190f374246c67df5d98ebee6
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-09-15 11:43:30 +0300

    fix bug

--- src/main/java/com/raif/javahack/javahack/controller/BusinessController.java
+++ src/main/java/com/raif/javahack/javahack/controller/BusinessController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 public class BusinessController implements BusinessApi{
-    @Atowired
+    @Autowired
     private BusinessService businessService;
 
     @GetMapping("/business/{id}")
